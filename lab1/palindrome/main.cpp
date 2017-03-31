512a53c13c33b1e530e6543d52d113603e62d070
Paweł Górski <pgorski42@gmail.com>
2017-03-31 21:27:24 +0200

    Created working implementation of 'is_palindrome' using string iterators.

---
+++ lab1/palindrome/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by pgorski42 on 31.03.17.
+//
+
+#include <iostream>
+#include <Palindrome.h>
+
+using namespace std;
+
+int main() {
+    cout << is_palindrome("12344321");
+    return 0;
+}
