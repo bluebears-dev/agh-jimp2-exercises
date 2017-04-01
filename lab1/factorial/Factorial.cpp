c9bc4d6afb0ff25d643f78182234058496ab044a
Paweł Górski <pgorski42@gmail.com>
2017-04-01 12:54:54 +0200

    Created 'factorial' lib.

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -3,6 +3,19 @@
 //
 #include "Factorial.h"
 
-int factorial(int value) {
-  return 0;
+
+int Factorial(int value) {
+    auto factorial = 1;
+
+    // abs(number) > 12 will overflow for int type
+    if (-12 <= value and value < 0)
+        for (auto i = value; i < 0; ++i)
+            factorial *= i;
+    else if (0 <= value and value <= 12)
+        for (auto i = 2; i <= value; ++i)
+            factorial *= i;
+    else
+        return 0;
+
+    return factorial;
 }
