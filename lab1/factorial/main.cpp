e7a504457464b90314b48599cf35db72b1f3975d
Paweł Górski <pgorski42@gmail.com>
2017-04-13 17:07:46 +0200

    Cleared branch of useless files.

--- lab1/factorial/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "Factorial.h"
-
-int main() {
-  std::cout << "Factorial of 20 is: " << Factorial(20) << std::endl;
-  return 0;
-}
--- lab2/xorcypherbreaker/main.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by pgorski42 on 01.04.17.
-//
